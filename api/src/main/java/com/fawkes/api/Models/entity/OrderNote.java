f70e9c0f82bca324be9a43b0cd50d5c8f0db7d5d
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-03-26 09:37:06 -0300

    fix: inserção de estrutura lombock e tradução do código

--- api/src/main/java/com/fawkes/api/Models/entity/OrderNote.java
+++ api/src/main/java/com/fawkes/api/Models/entity/OrderNote.java
@@ -2,7 +2,10 @@ package com.fawkes.api.Models.entity;
 
 import jakarta.persistence.*;
 import java.time.LocalDate;
-
+import lombok.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "orderNote")
 public class OrderNote {
@@ -12,24 +15,12 @@ public class OrderNote {
     private Integer id;
 
     @Column(name = "noteNumber", unique = true, length = 45, columnDefinition = "CHAR(45)")
-    private String numeroNota;
+    private String numberNote;
 
     @Column(name = "serie", length = 20)
     private String serie;
 
-    @Column(name = "orderDate")
-    private LocalDate dataEmissao;
+    @Column(name = "orderNoteDate")
+    private LocalDate orderNoteDate;
 
-    // Getters e Setters
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-
-    public String getNumeroNota() { return numeroNota; }
-    public void setNumeroNota(String numeroNota) { this.numeroNota = numeroNota; }
-
-    public String getSerie() { return serie; }
-    public void setSerie(String serie) { this.serie = serie; }
-
-    public LocalDate getDataEmissao() { return dataEmissao; }
-    public void setDataEmissao(LocalDate dataEmissao) { this.dataEmissao = dataEmissao; }
 }
