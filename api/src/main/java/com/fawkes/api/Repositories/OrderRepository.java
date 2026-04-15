3968c4af508c364a28361bfcf3be1f0b52ab6dc6
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-04-15 20:50:29 -0300

    feat:prototipagem do serviço de cadastro de pedidos

--- api/src/main/java/com/fawkes/api/Repositories/OrderRepository.java
+++ api/src/main/java/com/fawkes/api/Repositories/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.fawkes.api.Repositories;
 
-import com.fawkes.api.Entities.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.fawkes.api.Entities.Orders;
+
+
 @Repository
-public interface OrdersRepository extends JpaRepository<Orders, Long> {
+public interface OrderRepository extends JpaRepository<Orders, Long> {
+
 }
+
+    
+
