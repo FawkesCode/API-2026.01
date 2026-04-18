619168915ab030e709dcbd602aeb69e4a48b880b
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-04-18 09:49:18 -0300

    feat: criação de proteção contra inserção de notas fiscais duplicadas, com inserção de proteção de serie existente

--- api/src/main/java/com/fawkes/api/Repositories/OrderNoteRepository.java
+++ api/src/main/java/com/fawkes/api/Repositories/OrderNoteRepository.java
@@ -1,10 +1,11 @@
 package com.fawkes.api.Repositories;
 import com.fawkes.api.Entities.OrderNote;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderNoteRepository extends JpaRepository<OrderNote, Long> {
 
  boolean findByNumberNote(String numberNote);
+ boolean findBySerie(String serie);
+ 
 }
